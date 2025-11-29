10d03391d9f39d28fb1bcb8214b3dd40c5bc4c6f
XaydarovDev-BekzodBek <bekzodeoke@gmail.com>
2025-11-29 14:17:55 +0500

    changed app use for webhook

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ app.use("/api", GroupIdRouter);
 const StatsRouter = require("./routes/stats.route");
 app.use("/api", StatsRouter);
 
-app.use('web_hook_bot',bot.webhookCallback(WEBHOOK_PATH));
+app.use(bot.webhookCallback(WEBHOOK_PATH));
 
 // bot.launch({
 //   webhook: {
